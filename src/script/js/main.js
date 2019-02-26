5a465f4005876e20347718655ca8eb2a16f4193b
sadlessman <525441209@qq.com>
2019-02-26 08:47:04 +0800

    添加了基本效果

--- src/script/js/main.js
+++ src/script/js/main.js
@@ -1,4 +1,4 @@
 //调用模块
-require(['mode-effect']); 
+require(['mode-effect','mode-rendering']); 
 
 //require的跨域
--- src/script/js/mode-effect.js
+++ src/script/js/mode-effect.js
@@ -7,6 +7,7 @@ define(['config'], function() {
 		setTimeout(function () {
 			$changeimg.click();
 		},5000);
+		
 		$changeimg.on("click",function () {
 			if ($imgtop.height()!=60) {
 				$imgtop.animate({
@@ -27,5 +28,108 @@ define(['config'], function() {
 			}
 			
 		});
+		
+		
+		//悬浮二维码
+		
+		var $erweibtn=$('.top-right li:nth-of-type(5)');
+		var $bakbox=$('.top-right li:nth-of-type(5) .backbox');
+		var $erweicontent=$('.erweima');
+		
+		$erweibtn.hover(function () {
+			$bakbox.show();
+			$erweicontent.show();
+		},
+		function () {
+			$bakbox.hide();
+			$erweicontent.hide();
+		});
+		
+		
+		//轮播
+		
+		var $lunbo =$('.lunbo');
+		var $lunbouli =$('.lunbo ul li');
+		var $olbtns=$('.lunbo ol li');
+		var $lbleftb =$('.lunbo .left');
+		var $lbrightb =$('.lunbo .right');
+		var $timer=null;
+		var $autoplaytimer=null;
+		var $num=0;
+		var $numcolor=0;
+		
+		$lunbo.hover(function () {
+			$lbleftb.show();
+			$lbrightb.show();
+			clearInterval($autoplaytimer);
+		},function () {
+			$lbleftb.hide();
+			$lbrightb.hide();
+			$autoplaytimer=setInterval(function(){
+				$lbrightb.click();
+			},3000);
+		})
+		
+		$olbtns.hover(function () {
+			$numcolor=$(this).index();
+			$olbtns.eq($numcolor).css({"background":"#ee394b","opacity":"1"});
+		},function () {
+			$olbtns.eq($numcolor).css({"background":"#000","opacity":"0.4"});
+		})
+		
+		$olbtns.on('click',function () {
+			$num=$(this).index();
+			$olbtns.eq($num).css({"background":"#ee394b","opacity":"1"});
+			change();
+		})
+		
+		
+		
+		function change(){
+			$olbtns.eq($num).css({"background":"#ee394b","opacity":"1"}).siblings('li').css({"background":"#000","opacity":"0.4"});
+			$lunbouli.eq($num).animate({
+				opacity:1
+			}).siblings('li').animate({
+				opacity:0
+			});
+		}
+		
+		$lbleftb.hover(function () {
+			$lbleftb.css({"background-position":"-66px 0"});
+		},function () {
+			$lbleftb.css({"background-position":"0 0"});
+		});
+		$lbrightb.hover(function () {
+			$lbrightb.css({"background-position":"-66px 0"});
+		},function () {
+			$lbrightb.css({"background-position":"0 0"});
+		});
+		
+		$lbrightb.on('click',function () {
+			$num++;
+			if($num>$olbtns.length-1){
+				$num=0;
+			}
+			change();
+		});
+		
+		$autoplaytimer=setInterval(function(){
+			$lbrightb.click();
+		},3000);
+		
+		
+		
+		
+		//图片缩放
+		var $imgc=$('.imgscle img');
+		var $imgnum=0;
+		console.log($imgc);
+		$imgc.hover(function () {
+			$imgnum=$imgc.index($(this));
+			$imgc.eq($imgnum).css({"transform": "scale(1.1,1.1)"});
+			console.log($imgnum);
+		},function () {
+			$imgc.css({"transform": "scale(1,1)"});
+		})
 	})
 })
